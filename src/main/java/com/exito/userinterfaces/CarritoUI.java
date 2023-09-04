3a424e0d6f9181d43c03291bdffa438518a6ce53
Jessica Parra <42593273+JessicaParraG@users.noreply.github.com>
2023-09-04 08:25:33 -0500

    Reto Web Exito

--- src/main/java/com/exito/userinterfaces/CarritoUI.java
+++ src/main/java/com/exito/userinterfaces/CarritoUI.java
@@ -8,5 +8,6 @@ public class CarritoUI {
     public static final Target TXT_EMAIL = Target.the("Email").located(By.xpath("//input[@name='email']"));
     public static final Target BTN_CONFIRMAR = Target.the("Confirmar").located(By.xpath("//button[@class='exito-checkout-io-0-x-preLoginActiveButton']"));
     public static final Target BTN_MAS = Target.the("Boton aumentar cantidad").located(By.xpath("//div[@data-molecule-product-detail-quantity]//button/following-sibling::button"));
-    public static final Target LBL_CANTIDAD = Target.the("Cantidad de productos").located(By.xpath("//div[@class='exito-checkout-io-0-x-totalItemsSeller']"));
+    public static final Target LBL_CANTIDAD = Target.the("Cantidad de productos").located(By.xpath("//*[@class='exito-checkout-io-0-x-iconCloseActive ']"));
+    public static final Target LBL_TOTAL = Target.the("Total a pagar").located(By.xpath("//span[@class='exito-checkout-io-0-x-paymentButtonTextBold']"));
 }
